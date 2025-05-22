f060028de2532c0f30205be47146671460ae9020
susheelbms129 <susheel.is22@bmsce.ac.in>
2025-05-22 16:19:15 +0530

    Update apps.py

--- blood/apps.py
+++ blood/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
-
+from django.contrib.auth.models import User
+import logging
 
 class BloodConfig(AppConfig):
     name = 'blood'
+
+    def ready(self):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('susheel', 'susheel@gmail.com', 'CC123')
+            logging.info("Superuser created: susheel")
