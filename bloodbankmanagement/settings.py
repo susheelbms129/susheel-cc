2fd38e9fb2179fbd9f73fbb3b9f21e6f34d21f68
susheelbms129 <susheel.is22@bmsce.ac.in>
2025-05-22 16:20:44 +0530

    Update settings.py

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'widget_tweaks',
-    'blood',
+    'blood.apps.BloodConfig',
     'donor',
     'patient',
 ]
